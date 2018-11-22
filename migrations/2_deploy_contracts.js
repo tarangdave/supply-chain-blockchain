0caf033eb62e5d4029f7d73c22cc228b11f91d57
Tarang Dave <tarangadave@gmail.com>
2018-11-21 20:01:58 -0800

    remove old files

--- migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var SupplyChain = artifacts.require("./SupplyChain.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(SupplyChain);
-};
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SupplyChain from './SupplyChain';
-
-ReactDOM.render(
-  <SupplyChain />,
-  document.getElementById('root')
-);
